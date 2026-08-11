a452f0325a9771f05de44b87f4af39a49da1b645
Jithin <jithinjithudz@gmail.com>
2026-08-11 16:34:32 +0530

    fourth commit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-app-cache-v1";
+const CACHE_NAME = "policy-pulse-v1";
 
 const urlsToCache = [
   "./",
@@ -34,21 +34,36 @@ const urlsToCache = [
   "./js/utils.js",
   "./js/whatsapp.js",
 
-
   "./manifest.json",
 
   "./img/policy-pulse-192.png",
-  ".img/policy-pulse-512.png"
+  "./img/policy-pulse-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
