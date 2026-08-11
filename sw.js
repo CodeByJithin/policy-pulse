8ebe6265d2cf5cf3da77b8cc195e07cedce4f015
Jithin <jithinjithudz@gmail.com>
2026-08-11 18:12:25 +0530

    eighth commit

--- sw.js
+++ sw.js
@@ -1,11 +1,38 @@
-console.log("SW FILE LOADED");
+const CACHE_NAME = "policy-pulse-v3";
 
-self.addEventListener("install", () => {
-    console.log("SW INSTALL");
-    self.skipWaiting();
+self.addEventListener("install", event => {
+    console.log("Service worker installing...");
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.add("/");
+            })
+            .then(() => {
+                return self.skipWaiting();
+            })
+    );
 });
 
-self.addEventListener("activate", () => {
-    console.log("SW ACTIVATE");
-    self.clients.claim();
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        }).then(() => {
+            return self.clients.claim();
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
 });
